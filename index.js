ec8051320d9f6fbf449674ad48adf85cfe57017b
DianaMakovetskaya <diana.makovetska.knm.2019>
2021-06-22 11:48:51 +0300

    fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     var table = $('#table_id').DataTable( {
         "ajax": "data/objects.txt",
+        "order":[[1,"desc"]],
         "columns": [
             { data : "FirstName" },
             { data : "LastName" },
@@ -22,9 +23,9 @@ $(document).ready(function() {
     $('#table_id tbody').on( 'click', '#callback-button', function () {
         $('.modal').addClass('modal_active');
         const {Country, City,Street} = table.row( $(this).parents('tr') ).data();
-        $("#country").val(Country)
-        $("#city").val(City)
-        $("#street").val(Street)
+        $("#country").val(Country);
+        $("#city").val(City);
+        $("#street").val(Street);
         // let array = [];
         // array.push(table.row( $(this).parents('tr') ).data());
         //  $('#table_id2').DataTable( {
@@ -37,16 +38,17 @@ $(document).ready(function() {
         //         { data : "City" },
         //         { data : "Street" },]
         // } );
-        $('.modal__close-button').click(function () {
-            $('.modal').removeClass('modal_active');
-        });
-        $('.modal').mouseup(function (e) {
-            let modalContent = $(".modal__content");
-            if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
-                $(this).removeClass('modal_active');
-            }
-        });
+
     } );
+    $('.modal__close-button').click(function () {
+        $('.modal').removeClass('modal_active');
+    });
+    $('.modal').mouseup(function (e) {
+        let modalContent = $(".modal__content");
+        if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+            $(this).removeClass('modal_active');
+        }
+    });
 
         // $('#callback-button').click(function () {
         //     $('.modal').addClass('modal_active');
